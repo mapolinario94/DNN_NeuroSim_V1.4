517d679ef8c53ba451b5a90c421dfeb5e523d7b0
mapolina <marco.apolinario.l@gmail.com>
2025-04-15 17:01:15 -0400

    ResNet50

--- Inference_pytorch/inference.py
+++ Inference_pytorch/inference.py
@@ -79,7 +79,7 @@ elif args.dataset == 'imagenet':
 else:
     raise ValueError("Unknown dataset type")
     
-assert args.model in ['VGG8', 'DenseNet40', 'ResNet18'], args.model
+assert args.model in ['VGG8', 'DenseNet40', 'ResNet18', 'ResNet50'], args.model
 if args.model == 'VGG8':
     from models import VGG
     model_path = './log/VGG8.pth'   # WAGE mode pretrained model
